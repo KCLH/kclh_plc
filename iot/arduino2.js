6edd1ffc66d2b31bf42789ce515853593ce353d4
DongbinL <130536103+DongbinL@users.noreply.github.com>
2023-09-21 16:50:32 +0900

    arduino2 최종

--- iot/arduino2.js
+++ iot/arduino2.js
@@ -2,11 +2,7 @@ const { SerialPort } = require("serialport");
 const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://192.168.0.106:1884");
 const topic = "iot2/info";
-
-// 아두이노와 연결된 시리얼 포트 경로 (COM 포트 또는 /dev/ttyUSBx)
-const portPath = "COM9"; // 아두이노가 연결된 시리얼 포트 경로를 지정하세요.
-
-// 시리얼 포트 설정
+const portPath = "COM9";
 const serialPort = new SerialPort({
   path: portPath,
   autoOpen: false,
@@ -18,19 +14,15 @@ const serialPort = new SerialPort({
 serialPort.open(() => {
   console.log("connected");
 
-  // 데이터를 버퍼링할 변수 선언
   let bufferedData = "";
 
   serialPort.on("data", (data) => {
     const dataString = data.toString();
-    bufferedData += dataString; // 데이터를 버퍼에 추가
+    bufferedData += dataString; 
 
-    // 데이터가 끝났는지 확인
     if (bufferedData.includes("}]}")) {
-      // 데이터의 끝 패턴을 확인
       console.log("전체 데이터 수신 완료:", bufferedData);
 
-      // MQTT를 통해 전체 데이터 전송
       client.publish(topic, bufferedData, (err) => {
         if (err) {
           console.error("MQTT 전송 오류:", err);
@@ -39,7 +31,6 @@ serialPort.open(() => {
         }
       });
 
-      // 버퍼를 비웁니다.
       bufferedData = "";
     }
   });
